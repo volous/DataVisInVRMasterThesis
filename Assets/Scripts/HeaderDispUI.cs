d10632e23b83b16c6a21fca6a7dcd1b1fcce7846
tkl-cego <tkl@cego.dk>
2024-02-23 10:23:01 +0100

    packageDotGet

---
+++ Assets/Scripts/HeaderDispUI.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeaderDispUI : MonoBehaviour
+{
+    //String array for getting names of headers/features
+    public List<string> featureList = new List<string>();
+
+    //get canvas
+    [SerializeField] private Canvas _handUICanvas;
+    //getting object for inserting it into the UI, should grabbable be canvas within canvas, button or text?
+    [SerializeField] private Button _buttonPrefab;
+    
+    //function for initial population of features
+    private void InitFeatureOnCanvas()
+    {
+        List<Button> _newFeatureList = new List<Button>();
+        if (featureList.Count != 0)
+        {
+            float xMult = 1;
+            int ySub = 0;
+            for (int i = 0; i < featureList.Count; i++)
+            {
+                
+                //create objects based on how many features there are in the string list
+                Button _newFeature = Instantiate(_buttonPrefab);
+                _newFeature.transform.SetParent(_handUICanvas.transform, false);
+                //text on button is set to the list of features
+                _newFeature.GetComponentInChildren<TMPro.TextMeshProUGUI>().text = featureList[i];
+                _newFeatureList.Add(_newFeature);
+                //set positions, if amount of features is 9(some amount) or greater, start moving the features in the x direction
+                if (i % 9 == 0 && i!=0)
+                {
+                    xMult += 1.5f;
+                    ySub += 9;
+                }
+                _newFeatureList[i].transform.localPosition = new Vector3(-4+xMult, 4-(_newFeatureList[i].transform.position.y * i - (ySub * _newFeatureList[i].transform.position.y)), -0.5f);
+            }
+        }
+    }
+    
+    private void Start()
+    {
+        InitFeatureOnCanvas();
+    }
+}
