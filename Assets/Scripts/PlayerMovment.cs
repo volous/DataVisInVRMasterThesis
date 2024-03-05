d46a9260e469c330b979f5a3520437ada787e6c3
tkl-cego <tkl@cego.dk>
2024-03-05 09:23:44 +0100

    comments added to PlayerMovement script

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -6,28 +6,33 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovment : MonoBehaviour
 {
-    
+    //different variables:
+    //InputActionReference allows us to refer to the generated input map
     public InputActionReference triggerDown;
+    //starting movement speed
     public float movSpeed = 1;
-    [SerializeField] private GameObject _controllerObject;
-    [SerializeField] private Transform _facePos;
+    //reference to the controller object
+    [SerializeField] private GameObject _controllerObject; 
+    //reference to the reference object
     [SerializeField] private GameObject _selectedReference;
-    
-
+    //bool to determine if the user is moving
     private bool _isMoving = false;
     
     private void Start()
     {
+        //subscriptions to the function handling what happens on press and release
         triggerDown.action.performed += OnTriggerPress;
-        triggerDown.action.canceled += OnTriggerRelease;//Change
+        triggerDown.action.canceled += OnTriggerRelease;
     }
 
     private void Update()
     {
-        
+        //if the moving boolean is true
         if (_isMoving)
         {
+            //get the directinal difference between the controller and the reference object
             Vector3 _direction = (_controllerObject.transform.position - _selectedReference.transform.position);
+            //translation using the direciton
             transform.Translate(_direction * Time.deltaTime * movSpeed);
         }
         
@@ -35,31 +40,34 @@ public class PlayerMovment : MonoBehaviour
 
     private void OnDestroy()
     {
+        //unsubscribe to the press and release functions
         triggerDown.action.performed -= OnTriggerPress;
         triggerDown.action.canceled -= OnTriggerRelease;//change
     }
 
-    // Function to be triggered
     private void TriggerFunction()
     {
-        // Your code here
-        //Debug.Log("Trigger pressed!");
+        //sets bool to true
         _isMoving = true;
+        //sets the position of the reference object to be where the controller was, when the input was made
         _selectedReference.transform.position = _controllerObject.transform.position;
+        //activates the reference object, making it visible in the system
         _selectedReference.SetActive(true);
 
     }
     
     private void ReleaseFunction()
     {
-        //Debug.Log("Trigger Released!");
+        //sets bool to false
         _isMoving = false;
+        //deactivates the reference object
         _selectedReference.SetActive(false);
 
     }
     // Input action callback
     public void OnTriggerPress(InputAction.CallbackContext context)
     {
+        //runs the trigger function if the context has been subscribed to
         if (context.performed)
         {
             TriggerFunction();
@@ -68,6 +76,7 @@ public class PlayerMovment : MonoBehaviour
     
     public void OnTriggerRelease(InputAction.CallbackContext context)
     {
+        //runs the release function if the context has been subscribed to
         if (context.canceled)
         {
             ReleaseFunction();
